b0ad4e7ff53ff300e15f6a070b8dc7988ae15490
Brayan Ramos <brayanrcaicedo@gmail.com>
2024-05-24 22:27:33 -0500

    feat: add morgan middleware

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
 
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
-app.use(requestLogger)
+morgan.token('body', req => JSON.stringify(req.body))
+morgan(':method :url :status :res[content-length] - :response-time ms :body')
+app.use(morgan('tiny'))
 
 let persons = [
     {
